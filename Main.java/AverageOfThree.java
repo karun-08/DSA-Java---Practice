fe16a859de49a7767f04180f79cf16a077d0b33c
karun-08 <santhoshkarun843@gmail.com>
2026-08-25 22:10:22 +0530

    AverageOfThree

---
+++ Main.java/AverageOfThree.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int sum = a + b + c;
+    int avg = sum / 3;
+    System.out.println(avg);
+}
+}
