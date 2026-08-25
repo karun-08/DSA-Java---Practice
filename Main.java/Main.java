624fef025c51a8cbfb0190905561087fbbc25fdb
karun-08 <santhoshkarun843@gmail.com>
2026-08-25 21:47:34 +0530

    Main.java

---
+++ Main.java/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int sum =a+b+c;
+        System.out.println(sum);
+    }
+}
