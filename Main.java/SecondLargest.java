136eb2f243a3a122f370e561348177a39b98f0ed
karun-08 <santhoshkarun843@gmail.com>
2026-09-03 22:19:00 +0530

    SecondLargestNumber

---
+++ Main.java/SecondLargest.java
@@ -0,0 +1,23 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 8, 45, 30, 45};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("Second largest number: " + secondLargest);
+        }
+    }
+}
